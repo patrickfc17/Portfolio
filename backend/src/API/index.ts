55d81c4dd367dff355f952e4a9d2b892ca9e8bb1
Patrick Ferreira Carvalho <patrickferreiraC911@gmail.com>
2024-02-01 20:25:13 -0300

    Update index.ts

--- backend/src/API/index.ts
+++ backend/src/API/index.ts
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/api/', (req, res) => {
-    res.send({ hello: 'Hello Bitch :P' })
+    res.send({ hello: 'Hello There :P' })
 })
 
 app.listen(process.env.PORT, () => console.log(`Connection alive on http://localhost:${process.env.PORT}/api`))
